4a6bba8458ab240ef75b55b07439c607b7cad13b
Atharva <atharvamhaske76@gmail.com>
2025-12-04 23:53:02 +0530

    more context

--- context/main.go
+++ context/main.go
@@ -1,61 +1,58 @@
-package main
+// package main
 
+// import (
+// 	"fmt"
+// 	"net/http"
+// 	"time"
+// )
+
+// func hello(w http.ResponseWriter, req *http.Request) {
+// 	ctx := req.Context()
+// 	fmt.Println("server: hello handler started")
+// 	defer fmt.Println("server: hello handler ended")
+
+// 	select {
+// 	case <-time.After(10 * time.Second):
+// 		fmt.Fprintf(w, "hello\n")
+// 	case <-ctx.Done():
+
+// 		err := ctx.Err()
+// 		fmt.Println("server:", err)
+// 		internalError := http.StatusInternalServerError
+// 		http.Error(w, err.Error(), internalError)
+// 	}
+// }
+
+// func main() {
+// 	http.HandleFunc("/hello", hello)
+// 	http.ListenAndServe(":8090", nil)
+// }
+// 
+// example more below
+
+package main
 import (
+	"context"
 	"fmt"
-	"net/http"
 	"time"
 )
 
-func hello(w http.ResponseWriter, req *http.Request) {
-	ctx := req.Context()
-	fmt.Println("server: hello handler started")
-	defer fmt.Println("server: hello handler ended")
-
+func doWork(ctx context.Context) {
 	select {
-	case <-time.After(10 * time.Second):
-		fmt.Fprintf(w, "hello\n")
-	case <-ctx.Done():
-
-		err := ctx.Err()
-		fmt.Println("server:", err)
-		internalError := http.StatusInternalServerError
-		http.Error(w, err.Error(), internalError)
+		case <-time.After(2 * time.Second):
+		fmt.Println("work done")
+		case <-ctx.Done():
+		fmt.Println("cancelled:", ctx.Err())
 	}
 }
 
 func main() {
-	http.HandleFunc("/hello", hello)
-	http.ListenAndServe(":8090", nil)
+	ctx, cancel := context.WithTimeout(context.Background(), 1 * time.Second)
+	defer cancel() //cancel the context to release resources when done
+	
+	//start go routine to perform some processing
+	go doWork(ctx)
+	
+	//wait for a few seconds to allow proccessing to complete
+	time.Sleep(3 * time.Second)
 }
-
-// package main
-
-// import (
-//     "fmt"
-//     "net/http"
-//     "time"
-// )
-
-// func hello(w http.ResponseWriter, req *http.Request) {
-
-//     ctx := req.Context()
-//     fmt.Println("server: hello handler started")
-//     defer fmt.Println("server: hello handler ended")
-
-//     select {
-//     case <-time.After(10 * time.Second):
-//         fmt.Fprintf(w, "hello\n")
-//     case <-ctx.Done():
-
-//         err := ctx.Err()
-//         fmt.Println("server:", err)
-//         internalError := http.StatusInternalServerError
-//         http.Error(w, err.Error(), internalError)
-//     }
-// }
-
-// func main() {
-
-//     http.HandleFunc("/hello", hello)
-//     http.ListenAndServe(":8090", nil)
-// }
