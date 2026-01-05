49acc2486b87003db99cffbdc0132a7fc1f796da
atharvamhaske <atharvamhaske76@gmail.com>
2026-01-05 12:05:29 +0530

    atomic package practice

---
+++ fun-drills/atomic/main.go
@@ -0,0 +1,41 @@
+// package main
+
+// import (
+// 	"sync/atomic"
+// 	"sync"
+// 	"fmt"
+// )
+
+// func main() {
+// 	var x int32
+// 	var wg sync.WaitGroup
+// 	wg.Add(1)
+// 	go func() {
+// 		defer wg.Done()
+// 		atomic.AddInt32(&x, 2)
+// 		}()
+// 		wg.Wait()
+// 		fmt.Printf("This is new value of counter: %v \n", x)
+// }
+
+//now using channel
+
+package main
+
+import (
+	"fmt"
+	"sync/atomic"
+)
+
+func main() {
+	var x int32
+	done := make(chan struct{})
+
+	go func(){
+		atomic.AddInt32(&x,2)
+		close(done)
+	}()
+
+	<- done
+	fmt.Println("Value updated to:", x)
+}
